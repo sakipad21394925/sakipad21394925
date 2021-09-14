082b60688deb20c2c2b40b264adbc56480926983
sakipad21394925 <89963711+sakipad21394925@users.noreply.github.com>
2021-09-14 11:34:08 +0900

    Add files via upload

---
+++ mail_form.php
@@ -0,0 +1,51 @@
+<?php
+$action = $_POST['action'];
+$name = htmlspecialchars($_POST['name'],ENT_QUOTES);
+$email = htmlspecialchars($_POST['email'],ENT_QUOTES);
+$content = htmlspecialchars($_POST['content'],ENT_QUOTES);
+ 
+$error = '';
+if ($name == '') {
+    $error = $error.'<p>お名前が入力されていません。</p>';
+}
+if ($email == '') {
+    $error = $error.'<p>メールアドレスが入力されていません。</p>';
+}
+if ($content == '') {
+    $error = $error.'<p>内容が入力されていません。</p>';
+     
+    echo '<form method="post" action="form.html">';
+    echo '<input type="submit" name="backbtn" value="前のページへ戻る">';
+    echo '</form>';
+} else {
+    $mail .= "以下の内容が送信されました。\n\n";
+    $mail .= "【お名前】\n";
+    $mail .= $name."\n\n";
+    $mail .= "【メールアドレス】】\n";
+    $mail .= $email."\n\n";
+    $mail .= "【内容】\n";
+    $mail .= $content."\n\n";
+
+    mb_language("Japanese");
+    mb_internal_encoding("UTF-8");
+ 
+    $mail_to    = "form.jushin@gmail.com";
+    $mail_subject   = "メールフォームより送信されました";
+    $mail_body  = $mail;
+    $mail_header    = "from:".$email;
+     
+    $mailsend = mb_send_mail($mail_to, $mail_subject, $mail_body, $mail_header);
+     
+    if($mailsend == true) {
+        echo '<p>メールを送信しました。</p>';
+        echo '<form method="post" action="form.html">';
+        echo '<input type="submit" name="backbtn" value="前のページへ戻る">';
+        echo '</form>';
+    } else {
+        echo '<p>メール送信でエラーが発生しました。</p>';
+        echo '<form method="post" action="form.html">';
+        echo '<input type="submit" name="backbtn" value="前のページへ戻る">';
+        echo '</form>';
+    }
+}
+?>
